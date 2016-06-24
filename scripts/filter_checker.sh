2ff704f351a5408f90082f3b0a7eaf76bb7edc7c
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-06-24 16:24:21 +0500

    Fixed indent

--- scripts/filter_checker.sh
+++ scripts/filter_checker.sh
@@ -74,21 +74,21 @@ check_dns() {
 check_url() {
 	local file
 	local dir
-    local rc
+	local rc
 	dir="$DATADIR/$2"
 	file="$(mktemp $TMPDIR/XXXXXX)"
-    for METHOD in "$CURL" "$WGET"; do
-        $METHOD "$1" > $file 2>/dev/null
-        rc=$?                   # wget return 8 on 404
-        if [ "$rc" -gt 0 ] && ! [ "$rc" = "8" -a "$METHOD" = "$WGET" ]; then
-            echo "$1" >> $dir/2
-            rm -f $file
-            return
-        fi
-        head -c $FIRST_BYTES_FOR_CHECK "$file" | grep -q "$MARKER"
-        echo "$1" >> $dir/$?
-        rm -f $file
-    done
+	for METHOD in "$CURL" "$WGET"; do
+		$METHOD "$1" > $file 2>/dev/null
+		rc=$?                   # wget return 8 on 404
+		if [ "$rc" -gt 0 ] && ! [ "$rc" = "8" -a "$METHOD" = "$WGET" ]; then
+			echo "$1" >> $dir/2
+			rm -f $file
+			return
+		fi
+		head -c $FIRST_BYTES_FOR_CHECK "$file" | grep -q "$MARKER"
+		echo "$1" >> $dir/$?
+		rm -f $file
+	done
 }
 
 checker() {
@@ -108,8 +108,8 @@ checker() {
 		wait
 		show_report "$proto"
 	done < "$list"
-    wait # after break 2 we can have some background jobs
-    show_report "$proto"
+	wait # after break 2 we can have some background jobs
+	show_report "$proto"
 }
 
 http() {
