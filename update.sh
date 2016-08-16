cd96a809b1b2baa4b68cc0ba9109e4afc0e0c1ea
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-08-16 11:49:24 +0500

    Отмонтирование proc для обновления

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@
 BK=/root/backup.tar.gz
 
 $BINDIR/export.sh $BK
+umount /opt/reductor_satellite/proc || true
+umount /opt/reductor_satellite/dev  || true
 cd /opt/reductor_satellite_installer/
 git pull origin master
 ./install.sh
