bcbec73ed5f617c455e9f725a6a734c4b2300600
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-11-08 18:40:18 +0500

    Исправлено(обновление): не отмонтировывались dev и proc из чрута
    Был указан неправильный путь до них, maindir -> reductor_container

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@
 BK=/root/backup.tar.gz
 
 $BINDIR/export.sh $BK
-umount /opt/reductor_satellite/proc || true
-umount /opt/reductor_satellite/dev  || true
+umount /opt/reductor_satellite/reductor_container/proc || true
+umount /opt/reductor_satellite/reductor_container/dev  || true
 cd /opt/reductor_satellite_installer/
 git pull origin master
 ./install.sh
