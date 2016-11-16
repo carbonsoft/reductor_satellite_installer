26ddfb78a972c6ccb93da5ee96c0944d933761f9
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-11-16 12:56:49 +0600

    Добавлено(satellite): список пропущенных URL/доменов в отчётах

--- scripts/send_report.sh
+++ scripts/send_report.sh
@@ -11,11 +11,29 @@ To: $TO
 Content-Type: text/plain; charset = \"UTF-8\"
 Subject: Отчёт о фильтрации Reductor"
 
-echo "$HEADER" > /tmp/report
-
 russification() {
 	sed 's/ok/заблокировано/;s/fail/не заблокировано/;s/not open/не открывается/;s/total/всего/'
 }
 
+ru_names() {
+	sed -e 's|/opt/reductor_satellite/var.first|Первая проверка|g; s|/opt/reductor_satellite/var|Повторная проверка|g; s|/1||; s|/| |'
+}
+
+
+show_errors() {
+	echo
+	echo "# Список пропусков фильтрации:"
+	echo
+	for errors in $(find /opt/reductor_satellite/ -type f -name "1"); do
+		[ -s $errors ] || continue
+		echo
+		echo "## $errors"
+		echo
+		cat $errors
+	done | ru_names
+}
+
+echo "$HEADER" > /tmp/report
 russification < /opt/reductor_satellite/var/report | tr '#' '\n' >> /tmp/report
+show_errors >> /tmp/report
 sendmail -f $FROM -t $TO < /tmp/report
