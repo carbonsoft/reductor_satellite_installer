4a6fb2345e1cc06e6d472f25d8ee2e087592b134
nick-carbonsoft <n.denisov@carbonsoft.ru>
2017-01-10 11:31:45 +0600

    Исправлено(отчёт):отображение IP satellite
    При отправке отчета, игнорировались скобки

--- scripts/send_report.sh
+++ scripts/send_report.sh
@@ -6,7 +6,7 @@ IP="$(ip -o r g 8.8.8.8 | sed 's/.*src //; s/ .*//')"
 FROM=alarm@alarm.carbonsoft.ru
 TO="$1"
 
-HEADER="From: Reductor Satellite ($IP) <$FROM>
+HEADER="From: Reductor Satellite - $IP <$FROM>
 To: $TO
 Content-Type: text/plain; charset = \"UTF-8\"
 Subject: Отчёт о фильтрации Reductor"
