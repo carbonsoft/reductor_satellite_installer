c07495ec717145bc78cc74e6221f74931d07d1ba
root <moddingshell@gmail.com>
2017-03-07 10:06:05 +0500

    Исправлено(check_ip):не используем data
    CR-350

--- scripts/check_ip.sh
+++ scripts/check_ip.sh
@@ -6,64 +6,36 @@ declare -A netrc
 netrc['CURL']=0
 netrc['PING']=0
 
-declare -A datarc
-datarc['CURL']=0
-datarc['PING']=0
-
 net() {
 	local ip="$1"
 	local method="$2"
 	local dir="$3"
-	local file="$4"
 	local rc=0
-	${!method} "$ip" > $file 2>/dev/null || rc=$?
-	# shellcheck disable=SC2166
-	if [ "$rc" -gt 0 ]; then
-		echo "net_netrc" ${netrc[$method]} "net_rc" "$rc"
-		netrc[$method]="$rc"
-		rm -f $file
+	${!method} "$ip" > /dev/null 2>&1 || rc=$?
+	if [ "$rc" == 0 ]; then
+		netrc[$method]="1"
 		return "$rc"
 	fi
 	return 0
 }
 
-data() {
-	local ip="$1"
-	local method="$2"
-	local dir="$3"
-	local file="$4"
-	local rc=0
-	if [ "$rc" == '0' ]; then
-		echo "$(date) failed ${!method} $ip"
-		rc="1"
-	fi
-	datarc[$method]="$rc"
-	rm -f $file
-}
-
 analyze() {
-	if [ "${datarc['CURL']}" -gt 0 ] || [ "${datarc['PING']}" -gt 0 ]; then
+	if [ "${netrc['CURL']}" -gt 0 ] || [ "${netrc['PING']}" -gt 0 ]; then
 		return 1
-	elif [ "${datarc['CURL']}" = 0 ] && [ "${datarc['PING']}" = 0 ]; then
+	elif [ "${netrc['CURL']}" = 0 ] && [ "${netrc['PING']}" = 0 ]; then
 		return 0
 	else
 		# shellcheck disable=SC2046
-		echo $(date) Мы что-то не обработали $(set | egrep '^(data|net)rc')
+		echo $(date) Мы что-то не обработали $(set | egrep '^netrc')
 		return 3
 	fi
 }
 
 main() {
-	local rc
-	local file
 	local ip="$1"
 	local dir="$DATADIR/$2"
-	file="$(mktemp $TMPDIR/XXXXXX)"
 	for method in "CURL" "PING"; do
-		echo "main_method=" "$method"
-		if net "$ip" "$method" "$dir" "$file"; then
-			data "$ip" "$method" "$dir" "$file"
-		fi
+		net "$ip" "$method" "$dir" || netrc[$method]=$?
 	done
 	analyze
 }
