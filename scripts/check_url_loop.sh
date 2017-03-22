9aae2d9cc8357b9460e2990e83ba51e99aa77387
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-03-22 16:51:20 +0500

    Update check_url_loop.sh

--- scripts/check_url_loop.sh
+++ scripts/check_url_loop.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -eu
+
 . /opt/reductor_satellite/bin/filter_config.sh
 
+URL="$1"
+PROTO="${2:-http}"
 while sleep 0.001; do
-	DEBUG=${DEBUG:-0} /opt/reductor_satellite/bin/check_url.sh "$1" "${2:-http}" && echo -n + || exit 1
+	DEBUG=${DEBUG:-0} /opt/reductor_satellite/bin/check_url.sh "$URL" "$PROTO" && echo -n + || exit 1
 done
