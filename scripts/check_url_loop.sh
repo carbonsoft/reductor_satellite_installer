d7c29805d995d992ede87c0e1b0989e79edb8072
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-04-20 14:37:05 +0500

    Добавлено(цикличная проверка URL): более читаемый вывод о результатах проверки.

--- scripts/check_url_loop.sh
+++ scripts/check_url_loop.sh
@@ -6,6 +6,21 @@ set -eu
 
 URL="$1"
 PROTO="${2:-http}"
+LOG_INTERVAL="${LOG_INTERVAL:-100}"
+i=0
 while sleep 0.001; do
-	DEBUG=${DEBUG:-0} /opt/reductor_satellite/bin/check_url.sh "$URL" "$PROTO" && echo -n + || exit 1
+	rc=0
+	((i++)) || true
+	DEBUG=${DEBUG:-0} /opt/reductor_satellite/bin/check_url.sh "$URL" "$PROTO" || rc=$?
+	if [ "$rc" != '1' ]; then
+		if [ "$i" -gt "$LOG_INTERVAL" ]; then
+			echo $(date) "$LOG_INTERVAL" ok
+			i=0
+		fi
+	else
+		echo $(date) fail
+		if [ "${ASSERT:-1}" = '1' ]; then
+			exit 1
+		fi
+	fi
 done
